df02576efa98b94d3f2f16c9d81056cf4f12fb9b
lowkeyutsav <lowkeyutsav@gmail.com>
2026-07-02 06:00:03 +0545

    db schema implementated

---
+++ server/src/db/schema.ts
@@ -0,0 +1,44 @@
+import * as p from "drizzle-orm/pg-core";
+
+export const cryptocurrencies = p.pgTable("cryptocurrencies", {
+  id: p.uuid("id").defaultRandom(),
+  ticker: p.varchar("ticker", { length: 10 }).notNull().unique(),
+  name: p.varchar("name", { length: 50 }).notNull(),
+  currentPrice: p.numeric("current_price", { precision: 20, scale: 8 }).$type<
+    number
+  >(),
+  updatedAt: p.timestamp("updated_at", {
+    mode: "date",
+    withTimezone: true,
+  }).$onUpdate(() => new Date()),
+});
+
+export const triggerDirectionEnum = p.pgEnum("trigger_directions", [
+  "ABOVE",
+  "BELOW",
+]);
+
+export const priceAlerts = p.pgTable("price_alerts", {
+  id: p.uuid("id").defaultRandom(),
+  userId: p.text("user_id").notNull(),
+  cryptoId: p.integer("crypto_id").notNull().references(
+    () => cryptocurrencies.id,
+    { onDelete: "cascade" },
+  ),
+  targetPrice: p.numeric("target_price", { precision: 20, scale: 8 }).notNull()
+    .$type<number>(),
+  direction: triggerDirectionEnum(),
+  emailNotification: p.boolean("email_notification").default(true),
+  pushNotification: p.boolean("push_notification").default(false),
+  is_active: p.boolean("is_active").default(true),
+  is_triggered: p.boolean("is_triggered").default(false),
+  createdAt: p.timestamp("created_at", {
+    mode: "date",
+    withTimezone: true,
+  }).notNull().$default(() => new Date()),
+}, (table) => [
+  p.index("idx_active_alerts").on(
+    table.is_active,
+    table.is_triggered,
+  ),
+]);
